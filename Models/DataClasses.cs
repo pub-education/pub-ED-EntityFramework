dbf86c3961d91b4b213f1703dea221b3f976d0f0
ChrisJohDev <chris@chrisjohannesson.com>
2021-06-07 12:56:11 +0200

    Initial commit

---
+++ Models/DataClasses.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace EntityFramework.Models
+{
+    public class Student
+    {
+        public int Student_id { get; set; }
+        [Required(ErrorMessage = "Please, enter the first name.")]
+        public string FName { get; set; }
+        [Required(ErrorMessage = "Please, enter the last name.")]
+        public string LName { get; set; }
+    }
+
+    public class Teacher
+    {
+        public int Teacher_id { get; set; }
+        [Required(ErrorMessage ="Please, enter the first name.")]
+        public string FName { get; set; }
+        [Required(ErrorMessage = "Please, enter the last name.")]
+        public string LName { get; set; }
+    }
+
+    public class Course
+    {
+        public int Course_id { get; set; }
+
+        [Required(ErrorMessage ="Please, enter a course name.")]
+        public string Name { get; set; }
+    }
+
+    public class Assignment
+    {
+        public int Assignment_id { get; set; }
+        [Required(ErrorMessage ="Please, name the assignment.")]
+        public string Name { get; set; }
+    }
+
+    public class Students
+    {
+        public List<Student> StudentList { get; set; }
+    }
+    public class Teachers
+    {
+        public List<Teacher> TeacherList { get; set; }
+    }
+    public class Courses
+    {
+        public List<Course> CourseList { get; set; }
+    }
+    public class Assignments
+    {
+        public List<Assignment> AssignmentList { get; set; }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,28 @@
+﻿# Entity Framework
+
+Your assignment is to create an MVC application that connects to a database through Entity Framework. The application in this case is a school app – tracking teachers, students and courses, as well as assignments.
+
+
+## Required Features:
+* Should be able to add students, teachers, courses and assignments through the website
+* Should be able to assign students to courses, teachers and
+assignments to courses.
+* Should be able to print a course and see which assignments are tied
+to it, who the teacher is, and who the students are.
+* Data should be persistent between executions of the program.
+
+## Code Requirements:
+* A database must be created through Entity Framework, and connected to corresponding models in the C# code.
+* Sending data to and from the database should be done through an
+Entity Framework database context.
+* One-to-Many relationships need to be mutual relationships – an object reference on one side, and a list object on the other side.
+* Many-to-Many relationships need to be mutual in the C# code in the
+same way and be implemented in the database through association tables.
+
+## Resources:
+* https://app.pluralsight.com/library/courses/getting-started-entity-framework-core/table-of-contents
+* https://www.entityframeworktutorial.net/efcore/entity-framework-core.aspx
+* https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/working-with-linq
+
+
+Subjects Covered: Entity Framework, Code First
