2515b21673b8258c551f1bfe3bdcfd9fb2c1c822
ChrisJohDev <chris@chrisjohannesson.com>
2021-06-08 14:06:09 +0200

    SQL connection string fixed, relationships between classes/tables fixed.

--- Models/DataClasses.cs
+++ Models/DataClasses.cs
@@ -1,37 +1,74 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace EntityFramework.Models
 {
+
+    public class EntityFrameworkContext : DbContext
+    {
+        public EntityFrameworkContext(DbContextOptions<EntityFrameworkContext> options)
+            : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+    }
     public class Student
     {
+        [Key]
         public int Student_id { get; set; }
+
         [Required(ErrorMessage = "Please, enter the first name.")]
+        [MaxLength(45)]
         public string FName { get; set; }
+
         [Required(ErrorMessage = "Please, enter the last name.")]
+        [MaxLength(45)]
         public string LName { get; set; }
+        public List<Course> Courses { get; set; }
     }
 
     public class Teacher
     {
+        [Key]
         public int Teacher_id { get; set; }
+
+        [MaxLength(45)]
         [Required(ErrorMessage ="Please, enter the first name.")]
         public string FName { get; set; }
+
+        [MaxLength(45)]
         [Required(ErrorMessage = "Please, enter the last name.")]
         public string LName { get; set; }
+        [Required]
+        public List<Course> Courses { get; set; }
     }
 
     public class Course
     {
+        [Key]
         public int Course_id { get; set; }
+        [Required]
+        public int Teacher_id { get; set; }
 
+        public List<Student> Students { get; set; }
+        public List<Assignment> Assignments { get; set; }
+        [MaxLength(45)]
         [Required(ErrorMessage ="Please, enter a course name.")]
         public string Name { get; set; }
     }
 
     public class Assignment
     {
+        [Key]
         public int Assignment_id { get; set; }
+        [Required]
+        public int Course_id { get; set; }
+
+        [MaxLength(45)]
         [Required(ErrorMessage ="Please, name the assignment.")]
         public string Name { get; set; }
     }
--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using EntityFramework.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,11 @@ namespace EntityFramework
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<EntityFrameworkContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("EntityFrameworkContext")
+                    )
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
